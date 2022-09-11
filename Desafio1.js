fa84468fe5835c46770294b4dd4d0aabfbcaa8eb
LDamianC <62548781+LDamianC@users.noreply.github.com>
2022-09-11 20:28:10 -0300

    Add files via upload

---
+++ Desafio1.js
@@ -0,0 +1,8 @@
+let t, m, s, h
+t = prompt("Digite um tempo em segundos")
+console.log(t)
+m = Math.floor(t/60)
+s = t % 60
+h = Math.floor(m/60)
+m = m % 60
+console.log(t, " segundos são iguais a ", h," horas, ", m," minutos e ", s, " segundos")
