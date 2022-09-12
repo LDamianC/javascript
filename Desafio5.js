56f7a3e6acc44d6ccaa34d8a0aa3a4181d043c10
LDamianC <62548781+LDamianC@users.noreply.github.com>
2022-09-12 14:56:26 -0300

    Update Desafio5.js

--- Desafio5.js
+++ Desafio5.js
@@ -1,16 +1,23 @@
 let quantia, n100, n50, n20, n10, n5
+
 quantia=parseInt(prompt("Digite uma quantia de dinheiro para ser trocada e notas de 100, 50, 20, 10 e 5."))
 console.log(quantia)
+
 n100 = Math.floor(quantia/100)
 quantia = quantia%100
+
 n50 = Math.floor(quantia/50)
 quantia = quantia%50
+
 n20 = Math.floor(quantia/20)
 quantia = quantia%20
+
 n10 = Math.floor(quantia/10)
 quantia = quantia%10
+
 n5 = Math.floor(quantia/5)
 quantia = quantia%5
+
 if (quantia > 0){
   console.log("Esse valor não pode ser trocado.")
 } else {
