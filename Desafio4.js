130030b6625143a99a2eb1571429f933f674dcbf
LDamianC <62548781+LDamianC@users.noreply.github.com>
2022-09-11 21:52:30 -0300

    Add files via upload

---
+++ Desafio4.js
@@ -0,0 +1,8 @@
+let t1, t2, c1, c2, arealivre, pArea
+t1 = parseInt(prompt("Altura do terrreno:"))
+t2 = parseInt(prompt("Largura do terrreno:"))
+c1 = parseInt(prompt("Altura da casa:"))
+c2 = parseInt(prompt("Largura da casa:"))
+arealivre = t1*t2 - c1*c2
+pArea = (100*arealivre)/(t1*t2)
+console.log("Area livre: ", arealivre,"m² (", pArea,"%)")
